82f6c1f86d70516f44c5c445c0aaf73a88e5b727
Sac ML <sachin191ml@gmail.com>
2025-01-24 16:43:59 -0800

    [VS2022] Tutorial-17 (Multitexturing) VS2022 compatible

--- Tutorial 17_Multitexturing and Texture Arrays/Engine/d3dclass.h
+++ Tutorial 17_Multitexturing and Texture Arrays/Engine/d3dclass.h
@@ -8,19 +8,17 @@
 /////////////
 // LINKING //
 /////////////
-#pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3d11.lib")
-#pragma comment(lib, "d3dx11.lib")
-#pragma comment(lib, "d3dx10.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
 
 
 //////////////
 // INCLUDES //
 //////////////
-#include <dxgi.h>
-#include <d3dcommon.h>
 #include <d3d11.h>
-#include <d3dx10math.h>
+#include <directxmath.h>
+using namespace DirectX;
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -42,19 +40,25 @@ public:
 	ID3D11Device* GetDevice();
 	ID3D11DeviceContext* GetDeviceContext();
 
-	void GetProjectionMatrix(D3DXMATRIX&);
-	void GetWorldMatrix(D3DXMATRIX&);
-	void GetOrthoMatrix(D3DXMATRIX&);
+	void GetProjectionMatrix(XMMATRIX&);
+	void GetWorldMatrix(XMMATRIX&);
+	void GetOrthoMatrix(XMMATRIX&);
+
+	void GetVideoCardInfo(char*, int&);
+
+	void SetBackBufferRenderTarget();
+	void ResetViewport();
 
 	void TurnZBufferOn();
 	void TurnZBufferOff();
 
-	void TurnOnAlphaBlending();
-	void TurnOffAlphaBlending();
+	void EnableAlphaBlending();
+    void DisableAlphaBlending();
 
 private:
 	bool m_vsync_enabled;
-	
+	int m_videoCardMemory;
+	char m_videoCardDescription[128];
 	IDXGISwapChain* m_swapChain;
 	ID3D11Device* m_device;
 	ID3D11DeviceContext* m_deviceContext;
@@ -63,14 +67,13 @@ private:
 	ID3D11DepthStencilState* m_depthStencilState;
 	ID3D11DepthStencilView* m_depthStencilView;
 	ID3D11RasterizerState* m_rasterState;
-
-	D3DXMATRIX m_projectionMatrix;
-	D3DXMATRIX m_worldMatrix;
-	D3DXMATRIX m_orthoMatrix;
-
+	XMMATRIX m_projectionMatrix;
+	XMMATRIX m_worldMatrix;
+	XMMATRIX m_orthoMatrix;
+	D3D11_VIEWPORT m_viewport;
 	ID3D11DepthStencilState* m_depthDisabledStencilState;
 	ID3D11BlendState* m_alphaEnableBlendingState;
-	ID3D11BlendState* m_alphaDisableBlendingState;
+    ID3D11BlendState* m_alphaDisableBlendingState;
 };
 
 #endif
